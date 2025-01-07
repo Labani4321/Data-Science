5aad0ee22854dc42efeb1b2cfabe16269b514ad7
Labani Haldar <157562462+Labani4321@users.noreply.github.com>
2025-01-07 08:22:46 +0530

    Update Data Science.py

--- Data Science.py
+++ Data Science.py
@@ -2,4 +2,38 @@ print("labani")
   Labani
 student = "sam"
 student
-sam
+  sam
+student= "labani"
+student 
+  Labani
+a1= 22
+
+# Type
+a2 = 3+4j
+type (a2)
+  complex
+
+# Operators 
+a=32
+b=33
+a+b
+  56
+a-b
+ -1
+a/b
+  0.9696969696969697
+a==b
+  False
+a!=b
+  True
+a= True 
+b=False
+b & b 
+  False
+a & a
+  True
+a|b
+ True
+a|a
+  True
+
